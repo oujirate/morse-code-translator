b307e0553e5961cbd618be633c191c5a9da1a8e9
oujirate <oujireflect.me@gmail.com>
2024-07-26 16:18:29 +0700

    Morse Code Translator Oujirate

---
+++ morsecode.py
@@ -0,0 +1,60 @@
+import argparse
+import morsekit
+class CommandLine:
+    def run():
+        parser = argparse.ArgumentParser(
+            description="Morse Code Translate by oujirate",
+            usage='python %(prog)s [options] "<text/file>" -o "<output file>"'
+        )
+        group = parser.add_mutually_exclusive_group()
+
+        group.add_argument(
+            "-c", "--code",
+            type=str,
+            help="Code text to morse code",
+        )
+        group.add_argument(
+            "-d", "--decode",
+            type=str,
+            help="Decode morse code to text"
+        )
+        group.add_argument(
+            "-cf", "--codefile",
+            type=str,
+            help="Code text in file to morse code"
+        )
+        group.add_argument(
+            "-df", "--decodefile",
+            type=str,
+            help="Decode morse code in file to text"
+        )
+        parser.add_argument(
+            "-o", "--output",
+            type=str,
+            help="File output location"
+        )
+        args = parser.parse_args()
+
+        if args.code:
+            code = morsekit.core.code(args.code)
+            print(code) if code else None
+            
+        elif args.decode:
+            decode = morsekit.core.decode(args.decode)
+            print(decode) if decode else None
+
+        elif args.codefile:
+            if args.output:
+                morsekit.core.codefile(args.codefile,args.output)
+            else:
+                morsekit.core.codefile(args.codefile)
+        
+        elif args.decodefile:
+            if args.output:
+                morsekit.core.codefile(args.codefile,args.output)
+            else:
+                morsekit.core.decodefile(args.decodefile)
+        
+
+if __name__ == "__main__":
+    CommandLine.run()
---
+++ morsekit.py
@@ -0,0 +1,81 @@
+import json
+class core:
+    def code(text):
+        try:
+            result = ""
+            with open("morse-code.json","r") as mc:
+                morse = json.load(mc)
+                for i in text:
+                    if not text.isspace() and any(morse.keys()):
+                        if i.isspace():
+                            result += "/ "
+                            continue
+                        result += morse[i.lower()]
+                    else:
+                        raise ValueError("Morse code must be alphanumeric or one of following symbols (?,!,'.',',',';',:,+,-,/,=)")
+                    
+                    result += " "
+            return result
+
+        except Exception as ex:
+            print(f"Error: {ex}. Morse code must be alphanumeric or one of following symbols (?,!,'.',',',';',:,+,-,/,=)")
+
+    def decode(text):
+        try:
+            result = ""
+            with open("morse-code.json","r") as md:
+                morse = json.load(md)
+                morse_list = morse.items()
+
+                if not text.isspace():
+                    for word in text.split("/"):
+                        for value in word.split(" "):
+                            if value in morse.values():
+                                for keys,values in morse_list:
+                                    if value == values:
+                                        key = keys
+                                        result += key
+                            else:
+                                raise ValueError("Morse code not detected!")
+                        result += " "
+                    return result
+        except ValueError as ex:
+            print(f"Error: {ex}")
+
+    def codefile(file,path_file="output.txt"):
+        try:
+            result = ""
+            with open(file,"r") as txt:
+                textfile = txt.read()
+                for lines in textfile.split("\n"):
+                    if lines.isspace():
+                        continue
+                    line_code_txt = core.code(lines)
+                    result += line_code_txt
+                    result += "\n"
+
+            with open(path_file, "w") as txt:
+                txt.write(result)
+            print(f"Code successful! Save on '{path_file}'")
+
+        except Exception as ex:
+            print(f"Eror: {ex}")
+
+    def decodefile(file,path_file="output.txt"):
+        try:
+            result = ""
+            with open(file,"r") as txt:
+                textfile = txt.read()
+                for lines in textfile.split("\n"):
+                    if lines.isspace():
+                        continue
+                    line_decode_txt = core.decode(lines)
+                    result += line_decode_txt
+                    result += "\n"
+
+            with open(path_file, "w") as txt:
+                txt.write(result)
+            print(f"Code successful! Save on '{path_file}'")
+
+        except Exception as ex:
+            print(f"Eror: {ex}")
